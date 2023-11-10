a8121767d7da1e48bd050e3bbd5c51e1e93e75d2
Jesus <jesusdpdev@gmail.com>
2023-11-10 11:35:54 -0400

    first commit

---
+++ src/componentes/Boton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../hojas-de-estilo/boton.css'
+
+function Boton({ texto, esBotonDeClic, manejarClic }){
+  return (
+		<button className={ esBotonDeClic ? 'boton-click' : 'boton-reiniciar' } onClick={manejarClic}>
+			{texto}
+		</button>
+	)
+}
+
+export default Boton;
